1d38a5e47aa4bbb451f8fad1ee4e270a4c77ecca
Huynh Phan <huynhp24@gmail.com>
2022-04-17 19:26:55 -0700

    added a cool steak background with good stuff

--- food-zone/client/pages/index.tsx
+++ food-zone/client/pages/index.tsx
@@ -12,42 +12,29 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Image
+          className="inTheBack"
+          src="/images/steak.gif"
+          alt="Steak"
+          layout="fill"
+          objectFit="cover"
+        />
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to our <a href="https://nextjs.org">Food-Zone!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Have fun!
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className={styles.card}>
+            <h2>Recipe Maker &rarr;</h2>
+            <p>Create, Edit, and Save recipes!</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="" className={styles.card}>
+            <h2>Recipe List &rarr;</h2>
+            <p>Find your recipe! Yeehaw, shmeehaw!</p>
           </a>
         </div>
       </main>
